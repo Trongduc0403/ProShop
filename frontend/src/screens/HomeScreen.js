1d578d684946ca7f63c36c202b01a9dfe5190fb0
Trongduc0403 <trongduc0403@gmail.com>
2022-03-22 16:31:16 +0700

    ...

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,7 +6,7 @@ import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
-  //BUG
+
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
 
